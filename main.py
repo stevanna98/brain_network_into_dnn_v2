7b782f734ba43c553a363a330266d5e9958c3db4
Stefano Vannoni <s.vannoni98.lin@gmail.com>
2026-05-18 15:13:52 +0100

    fix: update subject_id in load_fc_matrix function for single sample selection

--- main.py
+++ main.py
@@ -143,7 +143,7 @@ def load_fc_matrix(path: str | None, n: int, sample: str) -> tuple[np.ndarray, s
 
     if sample == 'single':
         # subject_id = random.choice(list(data.keys()))
-        subject_id = '635245'
+        subject_id = '238033'
         fc = data[subject_id]['FC']
         sex = data[subject_id]['gender']
         age = data[subject_id]['age']
